3c2039a44da8996c4e35076cc10a614c38fa3598
dungdlvph13473 <dungdlvph13473@fpt.edu.vn>
2021-11-19 09:08:36 +0700

    https://github.com/dungdlvph13473/Du_An_1.git
    sd

---
+++ app/src/main/java/com/example/du_an_1_nhom_1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.du_an_1_nhom_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText text;
+    Button button;
+    Button đá;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
